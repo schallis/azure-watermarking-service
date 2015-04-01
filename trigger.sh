ed315f92548353455bfe59b412b25f292f00c49f
Steven Challis <steve@stevechallis.com>
2015-04-01 22:03:04 +0000

    Move email logic into upload script

--- trigger.sh
+++ trigger.sh
@@ -2,43 +2,35 @@
 INDIR=/srv/ftp/in
 OUTDIR=/srv/ftp/out
 
+log () {
+    logger "[$(date)] $@"
+};
+
 inotifywait -qme close_write -m --format %f $INDIR/ | while read filename; do
-    mime=`file --mime-type -b $INDIR/$filename`;
+    mime=`file --mime-type -b "$INDIR/$filename"`;
+    log "Found mime", $mime
 
     case $mime in
         'video/mp4')
-            echo $filename "is a video file", $mime
+            log "$filename is a video file $mime"
             # Warning: Colons in this string will cause the filter to fail
-            avconv -i $INDIR/$filename -y -strict experimental -f mp4 \
+            avconv -i "$INDIR/$filename" -y -strict experimental -f mp4 \
               -vf drawtext="fontfile=$INDIR/DejaVuSans.ttf:\
                             text='Exported on date %m.%d.%Y':\
                             fontcolor=white@0.5:\
                             fontsize=20:\
                             x=(W-w)/2:y=(H-h)/3:\
                             shadowx=3:shadowy=3:shadowcolor=black" \
-              $OUTDIR/$filename;
-              echo "Uploading..."
-              url=`python upload.py $filename`;
-              echo "Uploaded. Available at" $url
-              mail -s "Watermarked file available" "steve@stevechallis.com" <<EOF
-Hi,
-
-You requested a watermarked version of $filename which is now
-available for download at:
-
-$url
-
-Thank you,
-
-Your friendly Watermarking bot
-EOF
-              echo "Notified user via email"
+              "$OUTDIR/$filename";
+              log "Uploading..."
+              url=`python upload.py "$filename"`;
+              log "Uploaded. Available at" $url
             ;;
         'application/pdf')
-            echo $filename "is a PDF", $mime
+            log $filename "is a PDF", $mime
             ;;
         *)
-            echo $filename "not recognized", $mime
+            log $filename "not recognized", $mime
             ;;
     esac
 done
--- upload.py
+++ upload.py
@@ -1,21 +1,54 @@
 import sys
+import smtplib
+from email.mime.text import MIMEText
 
 from azure.storage import BlobService
 
 CONTAINER='watermarked'
 
 filename = sys.argv[1]
-blob_service = BlobService()
+SERVICE = BlobService()
 
-# Retrieve/create container
-if CONTAINER not in [c.name for c in blob_service.list_containers()]:
-    blob_service.create_container(CONTAINER)
+def ensure_container(name):
+    if name not in [c.name for c in SERVICE.list_containers()]:
+        SERVICE.create_container(name)
 
-# Upload
-blob_service.put_blob(CONTAINER, filename,
-        file('/srv/ftp/out/' + filename).read(), 'BlockBlob')
-url = blob_service.make_blob_url(CONTAINER, filename)
+def upload(container, filename):
+    SERVICE.put_blob(container, filename,
+            file('/srv/ftp/out/' + filename).read(), 'BlockBlob')
+    url = SERVICE.make_blob_url(container, filename)
+    return url
 
+def send_email(msg)
+    you = "steve@stevechallis.com"
+    me = "ZONZA Watermarking Bot <stevenchallis@warehouse-db.warehouse-db.b4.internal.cloudapp.net>"
+    msg = MIMEText(msg)
+    msg['Subject'] = "Watermarked file available"
+    msg['From'] = me
+    msg['To'] = you
+
+    s = smtplib.SMTP('localhost')
+    s.sendmail(me, [you], msg.as_string())
+    s.quit()
+
+ensure_container(CONTAINER)
+url = upload(CONTAINER, filename)
+
+msg = """
+Hi,
+
+You requested a watermarked version of $filename which is now
+available for download at:
+
+{0}
+
+Thank you,
+
+The ZONZA watermarking robot
+""".format(url)
+send_email(msg)
+
+# write URL to stdout for consumption by other apps
 sys.stdout.write(url)
 sys.stdout.flush()
 sys.exit(0)
